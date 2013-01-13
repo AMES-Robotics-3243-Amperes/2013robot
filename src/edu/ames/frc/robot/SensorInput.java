7ced0f81cd4d9ce9d67a1ec2c0cf2e3bbdde153f
Danial Ebling <daniale@scratchy>
2013-01-13 13:12:53 -0700

    commented some more

--- src/edu/ames/frc/robot/SensorInput.java
+++ src/edu/ames/frc/robot/SensorInput.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * 
- * This class polls robot sensors and switches.
+ * This class polls robot sensors and switches, such as gyros.
  */
 package edu.ames.frc.robot;
   
