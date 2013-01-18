a45b67057261d81a91f53fc17fee2dbf423cb45f
Kolton Yager <>
2013-01-18 11:20:40 -0700

    Basic Pin assignments in Robotmap

--- src/edu/ames/frc/robot/RobotMap.java
+++ src/edu/ames/frc/robot/RobotMap.java
@@ -7,6 +7,15 @@ package edu.ames.frc.robot;
 
 public class RobotMap {
     protected final static boolean debugmode = true;
-    protected static final double deadzone = .05;
+    protected final static double deadzone = .05;
+    /*Drive motor pins.
+     * For managment reasons we will track each wheel motor based on the side of the traingle/hexagon it is located at
+     * Each corner is labeled as either A,B, or C. A being the front wheel, B being the back right, and C being the right left.
+     * This nameing trend is reflected in the pin assignments in this class and should continue to be reflected in other parts
+     * of the overall robot system.
+     */
+    protected final static int Apin = 1; // Pin assignment for the front motor A
+    protected final static int Bpin = 2;
+    protected final static int Cpin = 3;
     
 } 
