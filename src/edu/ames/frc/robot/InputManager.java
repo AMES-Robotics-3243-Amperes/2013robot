fa7f4863b6be03df328924fcd8ccaed90cb6e07e
Kolton Yager <>
2013-01-18 16:39:55 -0700

    A ton of stuff for making the basic driving code.

--- src/edu/ames/frc/robot/InputManager.java
+++ src/edu/ames/frc/robot/InputManager.java
@@ -17,41 +17,75 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 
 public class InputManager {
-//Git is good
+//Git is good       double[] ABC = new double[3]; // Element 0 is the direction for A, the relative front end of the robot. The other two elements are arranged clockwise so the bottom right corner of the triangle is B and the other is C
+       // ABC[0] = 
     protected static Joystick PS2Cont = new Joystick(1);
     protected static RobotMap Rm = new RobotMap();
     protected static boolean dzactive  = false; // In case we want to check for deadzoneing being active
-    protected static double[][] GetPureAxis() { // Gets, stores, and returns the status of the joysticks on the PS2 Controller
+    
+    public static class directions{
+        public directions(double dira, double dirb, double dirc, double piv){
+            ABC[0] = dira;
+            ABC[1] = dirb;
+            ABC[2] = dirc;
+            pivot = piv;
+        }
+        double[] ABC = new double[3];//Holds directions
+        double pivot;
+    }
+    
+    public static double[] GetPureAxis() { // Gets, stores, and returns the status of the joysticks on the PS2 Controller
         /* We will use a double dimension arry to hold the joystick data so that everything can be sent to other functions.
          * Both of the first dimensions will hold 2 doulbes, the first is the x & y axis of the first (paning) joystick
          * The second dimension holds the x & y for the second (pivoting) joystick
          */
         double[][] axis = new double[2][2];// Variable for storing all that data
+        double[] pwr = new double [3];
         axis[0][0] = PS2Cont.getRawAxis(1);// X
         axis[0][1] = PS2Cont.getRawAxis(2);// Y
         axis[1][0] = PS2Cont.getRawAxis(3);// X
-        axis[1][1] = PS2Cont.getRawAxis(4);// Y
-        return (axis); // Returns axis data to the caller.
+ //       axis[1][1] = PS2Cont.getRawAxis(4);// Y We dont actually need this value
+        axis = deadzone(axis);
+        return (pwr); // Returns axis data to the caller.
     }
 
     protected static double[][] deadzone(double[][] axis) {// Checks for deadzone
         //This is a skeleton of the deadzone funtion. Mark should fill this in.
-        
-        
-        return (axis);
-    }
-    protected static double[][] ramp(double[][] axis){// Ramps inputs so that they curve all happy like.
-        //This is a skeleton of the ramp funtion. Mark should fill this in.
-        if(axis[0][0] <= Rm.deadzone){
+        if(axis[0][0] <= Rm.deadzone | axis[0][0] >= -Rm.deadzone){
             axis[0][0] = 0;
             dzactive = true;
         }
-        if(axis[0][1] <= Rm.deadzone){
+        if(axis[0][1] <= Rm.deadzone | axis[0][1] >= -Rm.deadzone){
+            axis[0][1] = 0;
+            dzactive = true;
     }
+        if(axis[1][0] <= Rm.deadzone | axis[1][0] >= -Rm.deadzone){
+            axis[1][0] = 0;
+            dzactive = true;
+        }
+ /*       if(axis[1][1] <= Rm.deadzone | axis[1][1] >= -Rm.deadzone){
+            axis[1][1] = 0;
+            dzactive = true;
+    }
+        */
         return (axis);
     }
-    protected static double[][] translate(double[][] axis){// Translates deadzoned and scaled inputs into whatever exact type of input MotorControl needs/wants.
+    protected static directions translate(double[][] axis){// Ramps inputs so that they curve all happy like.
+        //This is a skeleton of the ramp funtion. Mark should fill this in.
+        //Find direction
+        byte direction = 0; // 1, forward, 2 right
+        if(axis[0][1]> 0 ){
+            
+        }
+        double[] ABC = new double[3];
+        
+        directions dir;
+        return (dir);
+    }
+    //protected static double[] translate(double[][] axis){// Translates deadzoned and scaled inputs into whatever exact type of input MotorControl needs/wants.
+        
         //This is a skeleton of the translate funtion. Mark should fill this in.
-        return (axis);
-    }
+        
+        //return (ABC);
+   // }
 }
