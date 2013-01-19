8af93ba4144f8bb15205e65e0c8d528968c12064
Connie <Connie@Little_Mama>
2013-01-19 14:17:52 -0700

    Added my last name

--- src/edu/ames/frc/robot/FrisbeeSimulator.java
+++ src/edu/ames/frc/robot/FrisbeeSimulator.java
@@ -1,4 +1,4 @@
-/* Currently managed by Derick Anderson, Michael Chueng, and Hunter
+/* Currently managed by Derick Anderson, Michael Chueng, and Hunter Schmidt
  * This class uses exact physics to simulate the flight of the frissbee and ultimately returns
  * The angle of attack for shooting the frisbee from the turret. 
  * 
