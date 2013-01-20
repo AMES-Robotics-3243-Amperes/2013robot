93808f756583d6feba10e8e658d7dc1a0b8b55a8
Sowjanya.Yasa <Sowjanya.Yasa@I12300.octanner.com>
2013-01-20 14:48:09 -0700

    small comment

--- src/edu/ames/frc/robot/MotorControl.java
+++ src/edu/ames/frc/robot/MotorControl.java
@@ -78,7 +78,7 @@ public class MotorControl {
             col.set(Relay.Value.kOff);
         }
     }
-    
+    //the col motor either goes front, back or stays there.
     double[] convertHeadingToMotorCommands(double direction, double speed, double pivot) {
         double[] motorvalue = new double[3];
         
