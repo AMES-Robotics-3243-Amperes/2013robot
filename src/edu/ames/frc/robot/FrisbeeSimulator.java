ab360fb394d930834d9b462497c2bd59655b06ce
Danial Ebling <danialre@xmission.com>
2013-02-08 20:47:49 -0700

    Added basic trig to physics

--- src/edu/ames/frc/robot/FrisbeeSimulator.java
+++ src/edu/ames/frc/robot/FrisbeeSimulator.java
@@ -4,12 +4,12 @@
  * 
  */
 package edu.ames.frc.robot;
-
+import com.sun.squawk.util.MathUtils;
 public class FrisbeeSimulator {
 
-    public static double CalcPhys(double distance, double height) {
-        double finangle = 0;
-
-        return (finangle);
+    public static double BasicTrig(double distance, double height) {
+        double AoA = 0; // AoA is notation for Angle of Attack
+        AoA = MathUtils.atan2(height, distance);
+        return (AoA);
     }
 }
