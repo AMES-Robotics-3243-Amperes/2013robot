d080a68e77456782bb7471df73e40adbf2c81f96
Kolton Yager <>
2013-02-09 17:31:52 -0700

    Fixed ramping to be epic smooth butternesssssss

--- src/edu/ames/frc/robot/InputManager.java
+++ src/edu/ames/frc/robot/InputManager.java
@@ -86,15 +86,15 @@ public class InputManager {
 
     protected static double[] ramp(double[] axis) {
         for (byte ri = 0; ri < axis.length; ri++) {
-            axis[ri] = MathUtils.pow(axis[ri], rm.expo_ramp);
-           // axis[ri] = (2/3)*MathUtils.pow(axis[ri], RobotMap.expo_ramp)+(1/3)*axis[ri];
+            //axis[ri] = MathUtils.pow(axis[ri], rm.expo_ramp);
+            axis[ri] = ((.666)*MathUtils.pow(axis[ri], rm.expo_ramp))+((.333)*axis[ri]);
         }
         return (axis);
     }
     protected static double rampSingle(double axis) {
         
-            axis = MathUtils.pow(axis, rm.expo_ramp);
-           // axis[ri] = (2/3)*MathUtils.pow(axis[ri], RobotMap.expo_ramp)+(1/3)*axis[ri];
+            //axis = MathUtils.pow(axis, rm.expo_ramp);
+           axis = ((.666)*MathUtils.pow(axis, rm.expo_ramp))+((.333)*axis);
         return (axis);
     }
     protected static double[] translate(double[] axis) {// Translates final input values into a format for use by the rest of the code.
